bcf01a0c74d29a4ca8060ad2e074bbd2dc326022
Alexa404NL <66581333+Alexa404NL@users.noreply.github.com>
2026-05-09 21:45:37 -0600

    agregue assets para app y cambie paquete de firebase
    - editar view base

---
+++ Cardamomo/Cardamomo/View/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Cardamomo
+//
+//  Created by Alexa Lara on 09/05/26.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    @State private var isActive = false
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Button(action: {
+                    self.isActive = true }){
+                    Text("Go to Details")
+                }
+                NavigationLink(destination: Text("Detail View"), isActive: $isActive) {
+                    EmptyView()}
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
