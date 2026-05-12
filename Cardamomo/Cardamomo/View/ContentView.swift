f018498a96139f0f848c3d87057993aad56f4de8
Alexa404NL <66581333+Alexa404NL@users.noreply.github.com>
2026-05-11 19:02:47 -0600

    access a main view de ingredientes

--- Cardamomo/Cardamomo/View/ContentView.swift
+++ Cardamomo/Cardamomo/View/ContentView.swift
@@ -1,25 +1,9 @@
-//
-//  ContentView.swift
-//  Cardamomo
-//
-//  Created by Alexa Lara on 09/05/26.
-//
-
 import SwiftUI
-import SwiftData
 
 struct ContentView: View {
-    @State private var isActive = false
     var body: some View {
         NavigationStack {
-            VStack {
-                Button(action: { self.isActive = true}){
-                    Text("Go to Details")
-                }
-                NavigationLink(destination: Text("Detail View"), isActive: $isActive){
-                    EmptyView()
-                }
-            }
+            IngredientSelectionView()
         }
     }
 }
